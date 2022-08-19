44a45a550384b8ed616e8f1c147ee110bf74fc02
Christoph Schlameuss <schlac@users.noreply.github.com>
2022-08-19 08:32:45 +0200

    Add .gitconfig and fix setup script

--- setup.sh
+++ setup.sh
@@ -1,16 +1,23 @@
-#!/usr/bin/env bash
-set -e
+#!/usr/bin/bash
+set -ex
 
 create_symlinks() {
-config_dirs=("$@")
-for config_dir in "${config_dirs[@]}"; do
-    target_conf="${XDG_CONFIG_HOME:-~/.config}/$config_dir"
-    local_conf="$(realpath "$config_dir")"
-    [ -e ] && echo "SKIP '$target_conf' already exists" && continue
-    ln -s "$local_conf" "$target_conf" && echo "DONE created link '$target_conf'"
+target_dir="$1"
+shift
+for config in "$@"; do
+    target_conf="$target_dir"/"$config"
+    local_conf="$(realpath "$config")"
+    if test -e "$target_conf"; then
+        echo "SKIP '$target_conf' already exists"
+        continue
+    else
+        ln -s "$local_conf" "$target_conf" &&
+            echo "DONE created link '$target_conf'"
+    fi
 done
 }
 
 # ---
 
-create_symlinks nvim emacs
+create_symlinks "${HOME}" .gitconfig
+create_symlinks "${XDG_CONFIG_HOME:-${HOME}/.config}" nvim emacs
