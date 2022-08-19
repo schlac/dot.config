e79c215ebb4ede3082726d85f21b8c98864908be
Christoph Schlameuss <schlac@users.noreply.github.com>
2022-08-19 10:53:19 +0200

    Add aliases and improve setup to signal clearly if intened links are present

--- setup.sh
+++ setup.sh
@@ -7,17 +7,25 @@ shift
 for config in "$@"; do
     target_conf="$target_dir"/"$config"
     local_conf="$(realpath "$config")"
-    if test -e "$target_conf"; then
-        echo "SKIP '$target_conf' already exists"
+    if [[ -L "$target_conf" ]] ; then
+        if [[ "$(readlink "$target_conf")" == "$local_conf" ]] ; then
+            echo "SKIP '$target_conf' correct link already exists"
+        else
+            echo "WARN '$target_conf' already exists"
+        fi
         continue
-    else
-        ln -s "$local_conf" "$target_conf" &&
-            echo "DONE created link '$target_conf'"
     fi
+    if [[ -e "$target_conf" ]] || [[ -L "$target_conf" ]]; then
+        echo "WARN '$target_conf' already exists"
+        continue
+    fi
+
+    ln -s "$local_conf" "$target_conf" &&
+        echo "DONE created link '$target_conf'"
 done
 }
 
 # ---
 
-create_symlinks "${HOME}" .gitconfig
+create_symlinks "${HOME}" .aliases .gitconfig
 create_symlinks "${XDG_CONFIG_HOME:-${HOME}/.config}" nvim emacs
