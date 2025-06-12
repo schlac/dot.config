1e307f5bcc11bd59c47bc65e8d16cfbccc9740fa
Christoph Schlameuss <schlac@users.noreply.github.com>
2025-06-12 13:19:06 +0200

    Add easy mail macros

--- nvim/lua/macros.lua
+++ nvim/lua/macros.lua
@@ -1,3 +1,14 @@
+-- Easy mail answers
+local author = string.gsub(vim.fn.system { 'git', 'config', 'get', 'user.name' } .. " <" .. vim.fn.system { 'git', 'config', 'get', 'user.email' } .. ">", '\n', '')
+vim.keymap.set({"n", "v"}, "<leader>rr", ":append \nReviewed-by: " .. author .. "\n<c-c>")
+vim.keymap.set({"n", "v"}, "<leader>ra", ":append \nAcked-by: " .. author .. "\n<c-c>")
+vim.keymap.set({"n", "v"}, "<leader>rt", ":append \nTested-by: " .. author .. "\n<c-c>")
+vim.keymap.set({"n", "v"}, "<leader>rs", ":append \nSigned-off-by: " .. author .. "\n<c-c>")
+
+vim.keymap.set({"n", "v"}, "<leader>ss", ":append \n\n[...]\n\n<c-c>")
+
+vim.keymap.set({"n", "v"}, "<leader>rc", ":append \n--\nCheers,\n\nChristoph\n<c-c>")
+
 -- see https://stackoverflow.com/a/4468491 / https://github.com/junegunn/fzf.vim/pull/733
 function delte_buffer_keep_split()
   vim.cmd('bp|bd #')
