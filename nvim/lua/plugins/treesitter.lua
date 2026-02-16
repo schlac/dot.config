657a28d8d65ba9074a0947ec12ac0ae976e7455f
Christoph Schlameuss <schlac@users.noreply.github.com>
2026-02-16 20:16:41 +0100

    nvim treesitter context

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,6 @@
 return {
     -- syntax coloring
     {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate",},
+    -- context line
+    {"nvim-treesitter/nvim-treesitter-context", build = ":TSUpdate",},
 }
