0b96845b5674f6b84efc2957ab5c519c2c79147a
Christoph Schlameuss <schlac@users.noreply.github.com>
2026-02-17 14:43:32 +0100

    nvim obsidian config tuning

--- nvim/after/plugin/obsidian.lua
+++ nvim/after/plugin/obsidian.lua
@@ -8,7 +8,7 @@ if vim.fn.isdirectory("/home/chrschla/src/schlac/zettel/") == 0 then
     return
 end
 
-require'obsidian'.setup {
+require('obsidian').setup {
     legacy_commands = false,
     workspaces = {
         { name = "work", path = "~/src/schlameuss/notes/", },
@@ -23,14 +23,36 @@ require'obsidian'.setup {
         date_format = "%Y/%Y-%m",
         alias_format = "%Y-%m-%d",
     },
-    wiki_link_func = function(opts)
-        -- behave like use_path_only = true
-        return string.format("[[%s]]", opts.path)
+
+    -- Optional, customize how note IDs are generated given an optional title.
+    ---@param title string|?
+    ---@return string
+    note_id_func = function(title)
+      -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+      -- In this case a note with the title 'My new note' will be given an ID that looks
+      -- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'.
+      -- You may have as many periods in the note ID as you'd like—the ".md" will be added automatically
+      local suffix = ""
+      if title ~= nil then
+        -- If title is given, transform it into valid file name.
+        suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+      else
+        -- If title is nil, just add 4 random uppercase letters to the suffix.
+        for _ = 1, 4 do
+          suffix = suffix .. string.char(math.random(65, 90))
+        end
+      end
+      return tostring(os.date('%Y%m%d')) .. "-" .. suffix
     end,
+
+    -- wiki_link_func = function(opts)
+    --     -- behave like use_path_only = true
+    --     return string.format("[[%s]]", opts.path)
+    -- end,
     wiki_link_func = "use_alias_only",
     preferred_link_style = "wiki", -- Either 'wiki' or 'markdown'
 
-    disable_frontmatter = true,
+    -- disable_frontmatter = true,
     ui = {
         -- Define how various check-boxes are displayed
         checkbox = {
