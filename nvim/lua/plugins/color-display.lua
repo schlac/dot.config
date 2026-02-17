e4a57baac3346d551ad0bf75195aab4acce8faf0
Christoph Schlameuss <schlac@users.noreply.github.com>
2026-02-17 14:44:04 +0100

    nvim split highlight and color plugins

--- nvim/lua/plugins/color-display.lua
+++ nvim/lua/plugins/color-display.lua
@@ -1,5 +1,4 @@
 return {
-    {"tzachar/local-highlight.nvim", enabled = true},
     {"ap/vim-css-color", lazy = false, enabled = false,},
     {"norcalli/nvim-colorizer.lua",},
 }
---
+++ nvim/lua/plugins/highlight.lua
@@ -0,0 +1,5 @@
+return {
+    {"tzachar/local-highlight.nvim", enabled = true},
+    -- highlight words <leader>k
+    {"Mr-LLLLL/interestingwords.nvim", main = "interestingwords"},
+}
