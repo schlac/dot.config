a79e97c3e54b4844d222b5be502eac1f3cd5e8a4
Christoph Schlameuss <schlac@users.noreply.github.com>
2026-03-01 18:48:40 +0100

    fix nvim treesitter config

--- nvim/after/plugin/treesitter.lua
+++ nvim/after/plugin/treesitter.lua
@@ -1,4 +1,4 @@
-require'nvim-treesitter.configs'.setup {
+require'nvim-treesitter'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed) see https://github.com/nvim-treesitter/nvim-treesitter#supported-languages
   ensure_installed = {
       "bash",
